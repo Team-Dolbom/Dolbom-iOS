51831b9b87d5bf35103bddfe616ff319a1a1fee5
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-07-05 20:27:53 +0900

    [ADD] Community

--- iOS/Sources/Scenes/Community/CommunityViewController.swift
+++ iOS/Sources/Scenes/Community/CommunityViewController.swift
@@ -4,13 +4,72 @@ import Then
 import RxCocoa
 import RxSwift
 
-class CommunityViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        view.backgroundColor = .white
+class CommunityViewController: BaseViewController {
+    let viewModel = CommunityViewModel()
+    let communityList = PublishRelay<Void>()
+
+    let communityLabel = UILabel().then {
+        $0.text = "커뮤니티"
+        $0.font = UIFont.systemFont(ofSize: 30, weight: .medium)
     }
     let communityTableView = UITableView().then {
         $0.register(CommunityTableViewCell.self, forCellReuseIdentifier: "CommunityTableViewCell")
         $0.rowHeight = 93
+        $0.separatorStyle = .none
+    }
+
+    override func configureVC() {
+        communityTableView.delegate = self
+        communityTableView.dataSource = self
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        communityList.accept(())
+    }
+
+    override func bind() {
+        let input = CommunityViewModel.Input(communityList: communityList.asSignal(onErrorJustReturn: ()))
+        let output = viewModel.transform(input)
+        output.communityList.bind(to: communityTableView.rx.items(
+            cellIdentifier: "CommunityTableViewCell",
+            cellType: CommunityTableViewCell.self)
+        ) { _, item, cell in
+            cell.zoneLabel.text = "\(item.region)"
+            cell.checkLabel.text = item.view
+            cell.titleLabel.text = item.title
+        }
+    }
+
+    override func addView() {
+        [
+            communityLabel,
+            communityTableView
+        ].forEach {view.addSubview($0)}
+    }
+    override func setLayout() {
+        communityLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(71)
+            $0.leading.equalToSuperview().inset(12)
+            $0.trailing.equalToSuperview().inset(260)
+            $0.height.equalTo(44)
+        }
+        communityTableView.snp.makeConstraints {
+            $0.top.equalTo(communityLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(0)
+            $0.height.equalTo(650)
+        }
+    }
+}
+
+extension CommunityViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "CommunityTableViewCell",
+            for: indexPath) as? CommunityTableViewCell else { return UITableViewCell() }
+        return cell
     }
 }
--- iOS/Sources/Scenes/Community/CommunityViewModel.swift
+++ iOS/Sources/Scenes/Community/CommunityViewModel.swift
@@ -1,9 +1,30 @@
-//
-//  File.swift
-//  Dolbom-iOS
-//
-//  Created by 박도연 on 2023/07/05.
-//  Copyright © 2023 com.Dolbom. All rights reserved.
-//
-
 import Foundation
+import RxSwift
+import RxCocoa
+
+class CommunityViewModel: BaseViewModel {
+    private let disposeBag = DisposeBag()
+
+    struct Input {
+        let communityList: Signal<Void>
+    }
+    struct Output {
+        let communityList: BehaviorRelay<[Post]>
+    }
+
+    func transform(_ input: Input) -> Output {
+        let service = Service()
+        let communityList =  BehaviorRelay<[Post]>(value: [])
+        input.communityList.asObservable()
+            .flatMap { service.getCommunity() }
+            .subscribe(onNext: { data, res in
+                switch res {
+                case .okay:
+                    communityList.accept(data!.postList)
+                default:
+                    return
+                }
+            }).disposed(by: disposeBag)
+        return Output(communityList: communityList)
+    }
+}
