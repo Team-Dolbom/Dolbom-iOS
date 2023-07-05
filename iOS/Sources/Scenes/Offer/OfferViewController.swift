0e187d25a4d9939d4d7f4340ec0b30570169d05c
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-07-05 10:16:11 +0900

    [ADD] Tabbar

---
+++ iOS/Sources/Scenes/Offer/OfferViewController.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class OfferViewController: BaseViewController {
+    
+}
---
+++ iOS/Sources/Scenes/Tabbar/TabBarController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+        self.tabBar.unselectedItemTintColor = UIColor(named: "TabbarColor")
+        self.tabBar.tintColor = UIColor(named: "Main")
+    }
+    func setupTabBar() {
+        let communityView = CommunityViewController()
+        let offerView = OfferViewController()
+        let myPageView = MyPageViewController()
+
+        let communityImage = UIImage(named: "offer")?.withRenderingMode(.alwaysTemplate)
+
+        communityView.tabBarItem = UITabBarItem(title: "커뮤니티", image: communityImage
+                                                , tag: 0)
+        offerView.tabBarItem = UITabBarItem(title: "제안", image: UIImage(systemName: "house.fill"), tag: 1)
+        myPageView.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person.crop.circle.fill"), tag: 2)
+
+//        communityView.tabBarItem.selectedImage =
+
+        self.viewControllers = [communityView, offerView, myPageView]
+    }
+    
+}
